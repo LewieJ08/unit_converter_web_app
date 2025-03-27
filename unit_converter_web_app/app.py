5e630cef49165e2d49e51b1ddba1fa62bf3db4c7
LewieJ08 <lewiej08@gmail.com>
2025-03-27 12:44:59 +0000

    setup page rendering

--- unit_converter_web_app/app.py
+++ unit_converter_web_app/app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
-def main():
-    return "hello world"
+def index():
+    return render_template("index.html")
 
-print(main())
+@app.route("/length")
+
+def length():
+    return render_template("length.html")
+
+@app.route("/temperature")
+
+def temperture():
+    return render_template("temperature.html")
+
+@app.route("/weight")
+
+def length():
+    return render_template("weight.html")
 
 if __name__ == "__main__":
     app.run()
