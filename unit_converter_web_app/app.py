ac1f37ba189105c3f65954672627773e5e7ba3c7
LewieJ08 <lewiej08@gmail.com>
2025-03-29 12:27:55 +0000

    backend length convertion

--- unit_converter_web_app/app.py
+++ unit_converter_web_app/app.py
@@ -10,6 +10,27 @@ def index():
 @app.route("/length", methods=["GET", "POST"])
 
 def length():
+
+    length_conversions = {
+        "mm": 0.001,     
+        "cm": 0.01,        
+        "m": 1,          
+        "km": 1000,    
+        "in": 0.0254,     
+        "ft": 0.3048,     
+        "yd": 0.9144,   
+        "mi": 1609.34,
+     }
+        
+    if request.method == "POST":
+        unit = float(request.form["length"])
+        unit_for = request.form["unitFor"]
+        unit_to = request.form["unitTo"]
+
+        length_in_meters = unit * length_conversions[unit_for]
+        converted_length = length_in_meters / length_conversions[unit_to]
+
+
     return render_template("length.html")
 
 @app.route("/temperature", methods=["GET", "POST"])
