fe0e24eef14e8ba444738d87872f166c0033f0b4
LewieJ08 <lewiej08@gmail.com>
2025-03-29 19:42:12 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-
+    name="unit_converter_web_app",
+    version="1.0.0",
+    packages=find_packages(), 
+    install_requires=[
+        "flask"
+    ],  
+    entry_points={  
+        "console_scripts": [
+            "unit-converter=unit_converter_web_app.app" 
+        ]
+    },
+    author="Lewie Jackson",
+    author_email="LewieJ08@gmail.com",
+    description="A simple web app that allows the user to make unit convertions",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LewieJ08/unit_converter_web_app",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
