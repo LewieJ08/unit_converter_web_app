7ee47dc87bbb0ed7d2a7eb7285070d91d556bd08
LewieJ08 <lewiej08@gmail.com>
2025-03-31 13:36:45 +0100

    backend calculations

--- unit_converter_web_app/app.py
+++ unit_converter_web_app/app.py
@@ -2,14 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
 
-def index():
-    return render_template("index.html")
-
-@app.route("/length", methods=["GET", "POST"])
-
-def length():
+def convert_length(unit, unit_from, unit_to):
 
     length_conversions = {
         "mm": 0.001,     
@@ -22,6 +16,38 @@ def length():
         "mi": 1609.34,
      }
     
+    length_in_meters = unit * length_conversions[unit_from]
+    converted_length = length_in_meters / length_conversions[unit_to]
+
+    return converted_length
+    
+
+def convert_temperature(unit, unit_from, unit_to):
+        if unit_from == unit_to:
+            return unit
+
+        if unit_from == "C":
+            celsius = unit
+        elif unit_from == "F":
+            celsius = (unit - 32) * 5 / 9
+        elif unit_from == "K":
+            celsius = unit - 273.15
+
+        if unit_to == "C":
+            return celsius
+        elif unit_to == "F":
+            return (celsius * 9 / 5) + 32
+        elif unit_to == "K":
+            return celsius + 273.15
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+@app.route("/length", methods=["GET", "POST"])
+
+def length():
     if request.method == "POST":
         try:
             unit = float(request.form["length"])
@@ -30,13 +56,13 @@ def length():
 
             print(f"Calculating conversion: {unit} {unit_from} to {unit_to}...")
 
-            length_in_meters = unit * length_conversions[unit_from]
-            converted_length = length_in_meters / length_conversions[unit_to]
+            converted_length = convert_length(unit, unit_from, unit_to)
 
             return render_template("length.html", result = converted_length, unit_to = unit_to)
         
         except ValueError:
             print("Form field empty")
+            return render_template("length.html", result = None)
     
     else:
         return render_template("length.html", result = None)
@@ -44,9 +70,27 @@ def length():
 @app.route("/temperature", methods=["GET", "POST"])
 
 def temperature():
-    return render_template("temperature.html" , )
+
+    if request.method == "POST":
+        try:
+            unit = float(request.form["temperature"])
+            unit_from = request.form["unitFrom"]
+            unit_to = request.form["unitTo"]
+
+            print(f"Calculating conversion: {unit} {unit_from} to {unit_to}...")
+
+            converted_temperature = convert_temperature(unit, unit_from, unit_to)
+
+            return render_template("temperature.html", result = converted_temperature, unit_to = unit_to)
+
+        except ValueError:
+            print("Form field empty")
+            return render_template("temperature.html", result = None)
+
+    return render_template("temperature.html", result = None)
 
 @app.route("/weight",methods=["GET", "POST"])
+
 
 def weight():
     return render_template("weight.html")
