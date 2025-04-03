3cde313692f76be0ca5556fb10d86142d69552df
LewieJ08 <lewiej08@gmail.com>
2025-04-03 12:16:53 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="unit_converter_web_app",
     version="1.0.0",
-    packages=find_packages(), 
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "Flask"
     ],  
